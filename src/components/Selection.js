53e7f2469568de189deb6c4e3f5df33c98e7ab60
saif0110 <khanmohdtauheed4@gmail.com>
2024-11-26 14:23:11 +0530

    file changed

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,19 @@
+import React, { useState } from 'react';
+import '../styles/Child.css';
 
+const Selection = ({title, applyColor}) => {
+    const [updateSelectionStyle, setUpdateSelectionStyle] = useState({background: ""});
+    function handleClick(){
+        applyColor(setUpdateSelectionStyle)
+    }
+    console.log(title);
+  return (
+    <div>
+        <div style={{backgroundColor: updateSelectionStyle.background}} className='fix-box' onClick={handleClick}>
+            <p className='subheading'>{title}</p>
+        </div>
+    </div>
+  );
+}
+
+export default Selection;
